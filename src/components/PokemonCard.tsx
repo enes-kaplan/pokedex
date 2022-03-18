195dbc676660e379bfb35d6a6efa1516dd09a087
Enes Kaplan <eneskaplan94@hotmail.com>
2022-03-18 12:22:37 +0300

    Added loading state to PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -23,14 +23,16 @@ const PokemonCard = ({ url }: props) => {
     fetchData()
   }, [url])
 
-  return (
+  const card = (
     <div className='flex flex-col items-center p-6 bg-white rounded shadow-lg hover:shadow-2xl hover:translate-x-px hover:translate-y-px cursor-pointer transition-hover'>
-      <img src={pokemon?.image} alt={`Pokemon ${pokemon?.name}`} className="w-40 h-40" />
+      <img src={pokemon?.image ?? '/loader.svg'} alt={`Pokemon ${pokemon?.name}`} className="w-40 h-40" />
       <span className='text-3xl font-medium text-gray-600'>
-        {pokemon?.name}
+        {pokemon?.name ?? 'Loading...'}
       </span>
     </div>
   )
+
+  return card
 }
 
 export default PokemonCard
