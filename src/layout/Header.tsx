03c12616548e51b09f9ca29a972919ececa83f4d
Enes Kaplan <eneskaplan94@hotmail.com>
2022-03-18 16:14:24 +0300

    Added home link to header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
   return (
-    <div className="flex fixed inset-x-0 top-0 z-10 gap-4 justify-center items-center h-16 bg-white shadow-lg">
+    <Link to='/' className="flex fixed inset-x-0 top-0 z-10 gap-4 justify-center items-center h-16 bg-white shadow-lg">
       <img src="/pokeball.svg" alt="Pokeball Icon" className="w-10 h-10 select-none" />
       <span className="text-xl font-medium text-gray-600 select-none">
         Pokedex Application
       </span>
-    </div>
+    </Link>
   )
 }
 
