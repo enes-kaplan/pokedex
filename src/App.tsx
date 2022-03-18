45e4e7b13bc1decb08c1c2e64b97565e22df5042
Enes Kaplan <eneskaplan94@hotmail.com>
2022-03-18 12:45:23 +0300

    Implemented endless scrolling to pokemon list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { useState, useEffect } from 'react'
+import { axiosHandler } from './functions/axios'
 import Header from './layout/Header'
 import PokemonList from './components/PokemonList'
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [fetchUrl, setFetchUrl] = useState('https://pokeapi.co/api/v2/pokemon/?limit=25')
+  const [pokemonUrlList, setPokemonUrlList] = useState<string[]>([])
+
+  const fetchData = async() => {
+    setIsLoading(true)
+    console.log(fetchUrl)
+    const endpoint = fetchUrl.split('v2/')[1]
+    const [data, error] = await axiosHandler<any>(endpoint, 'GET')
+
+    if (!error) {
+      setPokemonUrlList((oldList) => {
+        const newList = data!.results.map((m: any) => m.url)
+        return [...oldList, ...newList]
+      })
+      setFetchUrl(data!.next)
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => { fetchData() }, [])
+
+  const scrollOffset = 150
+  // This function will set the value of position when the page is scrolled
+  const onScroll = (e: any) => {
+    const target = e.target as HTMLDivElement
+    // If closer to the bottom than specified offset, we should load new data
+    const shouldLoad = target.scrollHeight <= (target.scrollTop + target.clientHeight + scrollOffset)
+
+    if (shouldLoad && isLoading === false) {
+      fetchData()
+    }
+  }
+
   return (
-    <div className="overflow-y-auto pb-8 w-screen h-screen bg-slate-100">
+    <div onScroll={onScroll} className="overflow-y-auto pb-8 w-screen h-screen bg-slate-100">
       <Header />
-      <PokemonList />
+      <PokemonList pokemonUrlList={pokemonUrlList} />
     </div>
   )
 }
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,21 +1,10 @@
-import { useState, useEffect } from 'react'
-import { axiosHandler } from '../functions/axios'
 import PokemonCard from './PokemonCard'
 
-const PokemonList = () => {
-  const [pokemonUrlList, setPokemonUrlList] = useState<string[]>([])
+interface props {
+  pokemonUrlList: string[]
+}
 
-  useEffect(() => {
-    const fetchData = async() => {
-      const [data, error] = await axiosHandler<any>('pokemon/?limit=25', 'GET')
-      if (!error) {
-        setPokemonUrlList(data!.results.map((m: any) => m.url))
-      }
-    }
-
-    fetchData()
-  }, [])
-
+const PokemonList = ({ pokemonUrlList }: props) => {
   return (
     <div className='flex flex-wrap gap-8 justify-center px-8 mx-auto mt-8 max-w-7xl'>
       {pokemonUrlList.map((url, i) => <PokemonCard url={url} key={i} />)}
