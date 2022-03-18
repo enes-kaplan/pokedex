1a9e46453aa5334155958820a3be13f97ab624f2
Enes Kaplan <eneskaplan94@hotmail.com>
2022-03-18 12:34:06 +0300

    Replaced generic loader with pokedex loader gif

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -25,7 +25,10 @@ const PokemonCard = ({ url }: props) => {
 
   const card = (
     <div className='flex flex-col items-center p-6 bg-white rounded shadow-lg hover:shadow-2xl hover:translate-x-px hover:translate-y-px cursor-pointer transition-hover'>
-      <img src={pokemon?.image ?? '/loader.svg'} alt={`Pokemon ${pokemon?.name}`} className="w-40 h-40" />
+      {/* For pokedex loader, used this gif: https://dribbble.com/shots/14003796-Pok-dex-Pokemon-Pixel-Art */}
+      {/* Used this tool to remove background: https://onlinegiftools.com/remove-gif-background */}
+      {/* Used this tool to trim the empty spaces around: https://trimmy.io/ */}
+      <img src={pokemon?.image ?? '/loader.gif'} alt={`Pokemon ${pokemon?.name}`} className="w-40 h-40" />
       <span className='text-3xl font-medium text-gray-600'>
         {pokemon?.name ?? 'Loading...'}
       </span>
