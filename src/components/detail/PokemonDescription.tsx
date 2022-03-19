8c23313e871568782c4ff3105921fd58f3b2ff7c
Enes Kaplan <enes.kaplan@veribir.net>
2022-03-19 14:11:20 +0300

    Added multiplier tooltip to strengths/weaknesses

--- src/components/detail/PokemonDescription.tsx
+++ src/components/detail/PokemonDescription.tsx
@@ -53,8 +53,9 @@ const PokemonDescription = ({ pokemon, strength, weakness }: props) => {
           {strength?.map((type, sType) =>
             <div
               key={`strength-${sType}`}
-              className='py-1 px-2 font-bold text-center text-white rounded select-none text-shadow-type'
+              className='py-1 px-2 font-bold text-center text-white rounded cursor-help select-none text-shadow-type'
               style={{ backgroundColor: type.color }}
+              title={`Multiplier: ${type.multiplier}`}
             >
               {capitalizeFirstLetterOfEveryWord(type.name)}
             </div>
@@ -67,8 +68,9 @@ const PokemonDescription = ({ pokemon, strength, weakness }: props) => {
           {weakness?.map((type, wType) =>
             <div
               key={`strength-${wType}`}
-              className='py-1 px-2 font-bold text-center text-white rounded select-none text-shadow-type'
+              className='py-1 px-2 font-bold text-center text-white rounded cursor-help select-none text-shadow-type'
               style={{ backgroundColor: type.color }}
+              title={`Multiplier: ${type.multiplier}`}
             >
               {capitalizeFirstLetterOfEveryWord(type.name)}
             </div>
