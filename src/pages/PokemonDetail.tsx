e08053462a4b6568b3f45b5605f49a7b3a5a15e9
Enes Kaplan <enes.kaplan@veribir.net>
2022-03-20 13:40:03 +0300

    Added prev/next buttons to pokemon detail page

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -1,12 +1,14 @@
 import type { Pokemon, TypeRelation } from '../functions/types'
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { axiosHandler } from '../functions/axios'
 import { parsePokemon, getTypeRelations } from '../functions/common'
 import PokemonDescription from '../components/detail/PokemonDescription'
 
 const PokemonDetail = () => {
   const routeParams = useParams()
+  const navigate = useNavigate()
+
   const [pokemon, setPokemon] = useState<Pokemon>()
   const [strength, setStrengths] = useState<TypeRelation[]>()
   const [weakness, setWeakness] = useState<TypeRelation[]>()
@@ -29,7 +31,7 @@ const PokemonDetail = () => {
   }, [routeParams])
 
   return (
-    <div className='overflow-y-auto px-8 pb-16 mx-auto mt-8 max-w-7xl h-full'>
+    <div className='overflow-y-auto relative px-8 pb-16 mx-auto mt-8 max-w-7xl h-full hiddenScrollbar'>
       <div className='flex flex-col w-full sm:flex-row sm:justify-between sm:items-baseline'>
         <h1>{pokemon?.name}</h1>
         <h2>#{pokemon?.no}</h2>
@@ -38,6 +40,20 @@ const PokemonDetail = () => {
         <img src={pokemon?.image} alt={`Pokemon ${pokemon?.name}`} className='col-span-1 w-60 h-60' />
         <PokemonDescription pokemon={pokemon} strength={strength} weakness={weakness} />
       </div>
+      <button
+        className='flex fixed top-16 bottom-0 left-0 justify-center items-center w-16 text-7xl font-extrabold bg-gray-300 opacity-75 hover:opacity-100 disabled:opacity-20 disabled:cursor-not-allowed transition-hover h-content'
+        disabled={routeParams.no === '1'}
+        onClick={() => { navigate(`/pokemon/${parseInt(routeParams.no!) - 1}`, { replace: true }) }}
+      >
+        {'<'}
+      </button>
+      <button
+        className='flex fixed top-16 right-0 bottom-0 justify-center items-center w-16 text-7xl font-extrabold bg-gray-300 opacity-75 hover:opacity-100 disabled:opacity-20 disabled:cursor-not-allowed transition-hover h-content'
+        disabled={routeParams.no === '898'}
+        onClick={() => { navigate(`/pokemon/${parseInt(routeParams.no!) + 1}`, { replace: true }) }}
+      >
+        {'>'}
+      </button>
     </div>
   )
 }
