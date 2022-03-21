1a9491bff71a1250586be7ea2a479cff7ff10d14
Enes Kaplan <eneskaplan94@hotmail.com>
2022-03-21 14:25:58 +0300

    Fixed header for mobile

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,13 +5,15 @@ const Header = () => {
     <div className='flex fixed inset-x-0 top-0 z-10 justify-between items-center px-12 w-screen h-16 bg-white shadow-lg'>
       <Link to='/' className="flex gap-4 items-center">
         <img src="/pokeball.svg" alt="Pokeball Icon" className="self-center w-10 h-10 select-none" />
-        <span className="text-xl font-medium text-gray-600 select-none">
-        Pokedex Application
+        <span className='text-sm font-medium text-gray-600 select-none sm:text-xl'>
+          Pokedex Application
         </span>
       </Link>
-      <Link to='/whosthatpokemon' className='flex items-center text-xl font-medium text-gray-600 hover:underline transition-hover'>
+      <Link to='/whosthatpokemon' className='flex items-center hover:underline transition-hover'>
         <img src="/pokedex.png" alt="Pokedex Icon" className="self-center w-16 select-none" />
-        Who's that Pokemon?
+        <span className='text-sm font-medium text-gray-600 select-none sm:text-xl'>
+          Who's that Pokemon?
+        </span>
       </Link>
     </div>
   )
